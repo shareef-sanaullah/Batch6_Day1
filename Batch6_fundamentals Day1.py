d60b1349b98d03b5329083d5c8a78c1548f0d094
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-05 21:15:16 +0300

    Add files via upload

---
+++ Batch6_fundamentals Day1.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+student1 = 'Rafeeq'
+student2 = 'shareef'
+student3 = 'naveed'
+
+
+# In[1]:
+
+
+print(student1)
+
+
+# In[ ]:
+
+
+student1 = "shareef"
+student2 = "naveed"
+student3 = "rafeeq"
+
+
+# In[2]:
+
+
+print(student1)
+
+
+# In[3]:
+
+
+name = 'tony stark'
+
+print(name)
+
+
+# In[4]:
+
+
+student1 = 'shareef'
+student2 = 'naveed'
+student3 = 'rafeeq'
+
+print(student1)
+
+
+# In[5]:
+
+
+print(student2)
+
+
+# In[6]:
+
+
+name = 'sanaullah shareef'
+
+print(name)
+
+
+# In[7]:
+
+
+print(name.title())
+
+
+# In[8]:
+
+
+print(name.upper())
+
+
+# In[9]:
+
+
+print(name.lower())
+
+
+# In[ ]:
+
+
+
+
